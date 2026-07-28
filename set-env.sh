de20640df1dc4368b8cd89a81e9f43971bf49d11
Jeff Milton <milton@lajollalabs.com>
2026-07-28 09:02:34 -0700

    removerd open aid

--- set-env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Set and export environment variables
-export HOST_LINK="localhost:4200"
-#USED BY THE SERVER TO GENERATE LINKS FOR LINKEDIN AND NEWS ITEMS 
-export OPENAI_API_KEY="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"
